802acbfb485d328081e847ee276d799ce405496f
Treavor Gagne <gagne.16@hotmail.com>
2021-01-17 13:44:52 -0800

    Updates

---
+++ precache-manifest.793abef2cd0e118ed116dbd981c57a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e875589b52f6c669e1",
+    "url": "/portfolio/static/css/main.352ceff5.chunk.css"
+  },
+  {
+    "revision": "c0e875589b52f6c669e1",
+    "url": "/portfolio/static/js/main.c0e87558.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "eb12141010ba9bc9918106dfef8cd765",
+    "url": "/portfolio/index.html"
+  }
+];
